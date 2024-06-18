0740cf7c1e95ebe1a27342ce6dc10ffa83ae8c3d
Riyaz <91373561+Riyaz1000@users.noreply.github.com>
2024-06-18 15:37:01 +0530

    firstCommit

---
+++ src/githubpractice.java
@@ -0,0 +1,12 @@
+
+public class githubpractice {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        System.out.println("practicing git and github");
+    }
+
+}
+
+
